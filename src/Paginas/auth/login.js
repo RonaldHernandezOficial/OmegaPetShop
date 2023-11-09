a19e4c8291fa1973dcc451fd6716d6ddd1fe2ccb
unknown <Aprendiz@BOGDFPCGMF4100.FormacionSena.RED>
2023-11-09 16:58:09 -0500

    actualizao

--- src/Paginas/auth/login.js
+++ src/Paginas/auth/login.js
@@ -1,54 +1,74 @@
 import React from 'react';
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 const Login = () => {
+    const [formData, setFormData] = useState({
+        email: "",
+        password: "",
+    });
+    
+    const handleInputChange = (e) => {
+        const { name, value } = e.target;
+        setFormData({
+        ...formData,
+        [name]: value,
+        });
+    };
+    
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log("Datos enviados al Dashboard:", formData);
+        // Aquí puedes enviar los datos a tu Dashboard o cualquier otro lugar necesario
+    };
+    
     return (
         <div className="login-box">
-            <div className="login-logo">
-                <Link to="#"><b>Iniciar</b> sesión</Link>
-            </div>
-            {/* /.login-logo */}
-            <div className="card">
-                <div className="card-body login-card-body">
-                    <p className="login-box-msg">Bienvenido, ingrese sus credenciales</p>
-                    <form action="../../index3.html" method="post">
-                        <div className="input-group mb-3">
-                            <input type="email" 
-                            className="form-control" 
-                            placeholder="Email" 
-                            id = "email"
-                            name = "email"/>
-                            <div className="input-group-append">
-                                <div className="input-group-text">
-                                    <span className="fas fa-envelope" />
-                                </div>
-                            </div>
-                        </div>
-                        <div className="input-group mb-3">
-                            <input type="password" 
-                            className="form-control" 
-                            placeholder="Password" 
-                            id = "password"
-                            name = "password"/>
-                            <div className="input-group-append">
-                                <div className="input-group-text">
-                                    <span className="fas fa-lock" />
-                                </div>
-                            </div>
-                        </div>
-                    
-                    <div className="social-auth-links text-center mb-3">
-                        <Link to="/menu" className="btn btn-block btn-primary">
-                            <i className="#"/> Ingresar
-                        </Link>
-                        <Link to="" className="btn btn-block btn-danger">
-                            <i className="#"/> Crear cuenta
-                        </Link>
+        <div className="login-logo">
+            <b>Iniciar Sesión</b>
+        </div>
+        <div className="card">
+            <div className="card-body login-card-body">
+            <p className="login-box-msg">Bienvenido, ingrese sus credenciales</p>
+            <form onSubmit={handleSubmit}>
+                <div className="input-group mb-3">
+                <input
+                    type="email"
+                    className="form-control"
+                    placeholder="Email"
+                    name="email"
+                    value={formData.email}
+                    onChange={handleInputChange}
+                />
+                <div className="input-group-append">
+                    <div className="input-group-text">
+                    <span className="fas fa-envelope" />
                     </div>
-                    </form>
                 </div>
-                {/* /.login-card-body */}
+                </div>
+                <div className="input-group mb-3">
+                <input
+                    type="password"
+                    className="form-control"
+                    placeholder="Contraseña"
+                    name="password"
+                    value={formData.password}
+                    onChange={handleInputChange}
+                />
+                <div className="input-group-append">
+                    <div className="input-group-text">
+                    <span className="fas fa-lock" />
+                    </div>
+                </div>
+                </div>
+                <div className="social-auth-links text-center mb-3">
+                <button type="submit" className="btn btn-block btn-primary">
+                    Ingresar
+                </button>
+                </div>
+            </form>
             </div>
+        </div>
         </div>
 
     );
--- src/Paginas/menu.js
+++ src/Paginas/menu.js
@@ -1,103 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-
-
-
-const Home = () => {
-    return(
-        <div  className="wrapper">
-            <div className="content-wrapper">
-
-                <section  style={{backgroundColor : "#000000", backgroundImage:("radial-gradient(circle, #EAFAF1 4px, transparent 140px"),  backgroundSize: "20px 20px", backgroundPosition: "center center", backgroundAttachment: "fixed", margin: "40px"}}
-                className="content">
-                    
-                    <div img src= "#" className="container-fluid"></div>
-                        <div className="row">
-                        <div className="col-lg-3 col-6">
-                            <div className="small-box bg-info">
-                            <div style={{marginTop: "10px", backgroundColor: "#82E0AA", borderColor : "#82E0AA", color: "black"}} className="inner">
-                            <h3>Comida perritos !</h3>
-                        </div>
-                        <div className="icon">
-                            <i className="fa fa-home"></i>
-                        </div>
-                        <Link to={"/productosPet"} className='small-box-footer'>Ver Productos <i className='fas fa-arrow-circle-right'></i></Link>
-                        </div>
-                        <p style={{textAlign: "center", textAlignLast:"inherit"}}>Aqui encontraras diferentes tipos de comida para tu mascota, igualmente por diferente cantidad </p>
-                        </div>
-                        </div>
-                    </section>
-
-                    <section style={{backgroundColor : "#000000", backgroundImage:("radial-gradient(circle, #EAFAF1 4px, transparent 140px"),  backgroundSize: "20px 20px", backgroundPosition: "center center", backgroundAttachment: "fixed", margin: "40px"}}
-                    className="content" >
-                    <div className="container-fluid"></div>
-                        <div className="row">
-                        <div className="col-lg-3 col-6">
-                            <div className="small-box bg-info">
-                            <div style={{marginTop: "10px", backgroundColor: "#82E0AA", borderColor : "#82E0AA", color: "black"}} className="inner">
-                            <h3>Juguetes perritos !</h3>
-
-                            <p>&nbsp;</p>
-                        </div>
-                        <div className="icon">
-                            <i className="fa fa-home"></i>
-                        </div>
-                        <Link to={"/productosPet"} className='small-box-footer'>Ver Productos <i className='fas fa-arrow-circle-right'></i></Link>
-                        </div>
-                        <p style={{textAlign: "center", textAlignLast:"inherit"}}>Aqui encontraras diferentes tipos de comida para tu mascota, igualmente por diferente cantidad </p>
-                        </div>
-                        </div>
-                    </section>
-
-                    <section style={{backgroundColor : "#000000", backgroundImage:("radial-gradient(circle, #EAFAF1 4px, transparent 140px"),  backgroundSize: "20px 20px", backgroundPosition: "center center", backgroundAttachment: "fixed", margin: "40px"}}
-                     className="content">
-                    <div className="container-fluid"></div>
-                        <div className="row">
-                        <div className="col-lg-3 col-6">
-                            <div className="small-box bg-info">
-                            <div style={{marginTop: "10px", backgroundColor: "#82E0AA", borderColor : "#82E0AA", color: "black"}} className="inner">
-                            <h3>Accesorios perritos !</h3>
-
-                            <p>&nbsp;</p>
-                        </div>
-                        <div className="icon">
-                            <i className="fa fa-home"></i>
-                        </div>
-                        <Link to={"/productosPet"} className='small-box-footer'>Ver Productos <i className='fas fa-arrow-circle-right'></i></Link>
-                        </div>
-                        <p style={{textAlign: "center", textAlignLast:"inherit"}}>Aqui encontraras diferentes tipos de comida para tu mascota, igualmente por diferente cantidad </p>
-                        </div>
-                        </div>
-                    </section>
-
-                    <section style={{backgroundColor : "#000000", backgroundImage:("radial-gradient(circle, #EAFAF1 4px, transparent 140px"),  backgroundSize: "20px 20px", backgroundPosition: "center center", backgroundAttachment: "fixed", margin: "40px"}}
-                    className="content">
-                    <div className="container-fluid"></div>
-                        <div className="row">
-                        <div className="col-lg-3 col-6">
-                            <div className="small-box bg-info">
-                            <div style={{marginTop: "10px", backgroundColor: "#82E0AA", borderColor : "#82E0AA", color: "black"}} className="inner">
-                            <h3>Aseo para perritos !</h3>
-
-                            <p>&nbsp;</p>
-                        </div>
-                        <div className="icon">
-                            <i className="fa fa-home"></i>
-                        </div>
-                        <Link to={"/productosPet"} className='small-box-footer'>Ver Productos <i className='fas fa-arrow-circle-right'></i></Link>
-                        </div>
-                        <p style={{textAlign: "center", textAlignLast:"inherit"}}>Aqui encontraras diferentes tipos de comida para tu mascota, igualmente por diferente cantidad </p>
-                        </div>
-                        </div>
-                    </section>
-
-                    
-
-                    
-                </div>
-        </div>
+const Menu = () => {
+    return (
+        <div>Hola</div>
+        
     )
 }
 
-export default Home;
+export default Menu;
